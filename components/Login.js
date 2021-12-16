acb48fef0cf34ff22ba9c5ae221398806b1ef929
weshsnr <ndungugeoffrey11@gmail.com>
2021-12-16 11:40:34 +0300

    added login component

---
+++ components/Login.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+
+const Login = () => {
+    return (
+        <div className="bg-black relative" >
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4 " >
+
+                <Image src="https://links.papareact.com/3pi" width={200} height={200} className="object-cover rounded-full"
+                />
+
+                <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse ">Login into the Metaverse</button>
+            </div>
+
+            <div className="w-full h-screen" >
+                <Image src="https://links.papareact.com/55n" layout='fill' objectFit='cover'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Login
